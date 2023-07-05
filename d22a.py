bbb7b488a8aff4de54277b2b88d374d0841942db
grandey_t1 <grandey@users.noreply.github.com>
2023-07-05 18:22:12 +0800

    get_calendar_days_branch(): Get calendar, number of days in year, and parent branch year for historical simulation of an ESM.

--- d22a.py
+++ d22a.py
@@ -10,6 +10,7 @@ from functools import cache
 import matplotlib.pyplot as plt
 import pathlib
 from watermark import watermark
+import xarray as xr
 
 
 # Matplotlib settings
@@ -48,3 +49,32 @@ def get_watermark():
     """Return watermark string, including versions of dependencies."""
     packages = 'matplotlib,numpy,pandas,scipy,seaborn,statsmodels,xarray'
     return watermark(machine=True, conda=True, python=True, packages=packages)
+
+
+@cache
+def get_calendar_days_branch(esm='UKESM1-0-LL_r1i1p1f2'):
+    """Get calendar, number of days in year, and parent branch year for historical simulation of an ESM."""
+    # Read historical Dataset
+    in_fns = sorted(IN_BASE.glob(f'*/zostoga/{esm}/*_{esm}_historical.mergetime.nc'))
+    if len(in_fns) != 1:
+        print(f'et_calendar_ndays_branch({esm}): {len(in_fns)} files found for zostoga; using {in_fns[0]}')
+    in_ds = xr.open_dataset(in_fns[0])
+    # Calendar
+    calendar = in_ds['time'].attrs['calendar']
+    # Number of days in year
+    if '_day' in calendar:
+        days_in_yr = int(calendar[0:3])
+    elif 'gregorian' in calendar:
+        days_in_yr = 365.25
+    else:
+        print(f'get_calendar_days_branch({esm}): calendar {calendar} not recognised; assuming 365.25 days in year')
+        days_in_yr = 365.25
+    # Branch year
+    parent_time_units = in_ds.attrs['parent_time_units']
+    branch_time_in_parent = in_ds.attrs['branch_time_in_parent']
+    if parent_time_units[0:10] == 'days since':
+        branch_yr = round(int(parent_time_units[11:15]) + float(branch_time_in_parent) / days_in_yr)
+    else:
+        print(f'get_calendar_days_branch({esm}): parent_time_units {parent_time_units} not recognised')
+        branch_yr = None
+    return calendar, days_in_yr, branch_yr
