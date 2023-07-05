3e1f6ec8e38dc048d5e4ff6e4edd9f555fb22972
grandey_t1 <grandey@users.noreply.github.com>
2023-07-05 17:22:40 +0800

    d22a.py: add IN_BASE, SYMBOLS_DICT, and UNITS_DICT.

--- d22a.py
+++ d22a.py
@@ -6,8 +6,9 @@ Author:
     Benjamin S. Grandey, 2022-2023.
 """
 
+from functools import cache
 import matplotlib.pyplot as plt
-import seaborn as sns
+import pathlib
 from watermark import watermark
 
 
@@ -20,11 +21,28 @@ plt.rcParams['axes.labelcolor'] = '0.15'
 plt.rcParams['mathtext.default'] = 'default'
 plt.rcParams['savefig.dpi'] = 300
 
-# Seaborn style
-sns.set_style('whitegrid')
 
+# Constants
+IN_BASE = pathlib.Path.cwd() / 'data'  # base directory of input data produced by data_d22a.ipynb
 
-# Watermark, including versions of dependencies
+SYMBOLS_DICT = {'Ep': "$E \prime$",  # TOA radiative flux
+                'E': "$\Delta E$",  # change in Earth system energy
+                'Hp': "$H \prime$",  # sea-surface heat flux
+                'H': "$\Delta H$",  # change in ocean heat content
+                'Z': "$\Delta Z$",  # change in thermosteric sea level
+                'eta': "$\eta$",  # fraction of excess energy absorbed by the ocean
+                'eps': "$\epsilon$",  # ocean expansion efficiency of heat
+                }
+
+UNITS_DICT = {'Ep': "W m$^{-2}$",
+              'E': "YJ",
+              'Hp': "W m$^{-2}$",
+              'H': "YJ",
+              'Z': "mm",
+              'eta': "unitless",
+              'eps': "mm YJ$^{-1}$",
+              }
+
 
 def get_watermark():
     """Return watermark string, including versions of dependencies."""
